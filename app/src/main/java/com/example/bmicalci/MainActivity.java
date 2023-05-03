98c0414c71465e21f7220cd713dea8beb5b63baf
Ved Jaiswal <vedjaiswal0506@gmail.com>
2023-05-04 00:53:09 +0530

    add logic

--- app/src/main/java/com/example/bmicalci/MainActivity.java
+++ app/src/main/java/com/example/bmicalci/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.bmicalci;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,5 +17,33 @@ public class MainActivity extends AppCompatActivity {
         EditText weight, heightF, heightI;
         Button calculate;
         TextView result;
+        weight = findViewById(R.id.editWeight);
+        heightF = findViewById(R.id.editHeightFt);
+        heightI = findViewById(R.id.editHeightIn);
+        calculate = findViewById(R.id.buttonCalculate);
+        result = findViewById(R.id.textResult);
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int wt = Integer.parseInt(weight.getText().toString());
+                int ft = Integer.parseInt(heightF.getText().toString());
+                int in = Integer.parseInt(heightI.getText().toString());
+                int total = ft*12 + in;
+                double cm = total*2.53;
+                double meters = cm/1000;
+                double bmi = wt/(meters*meters);
+
+                if(bmi > 25){
+                    result.setText(R.string.textOW);
+                }
+                else if(bmi < 18){
+                    result.setText(R.string.textUW);
+                }
+                else{
+                    result.setText(R.string.textH);
+                }
+            }
+        });
     }
 }
